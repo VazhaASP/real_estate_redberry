5e1fe9b20551663fbe6afcaa2027920ecc4366a7
VazhaASP <vazhaaspanidze12@gmail.com>
2024-09-18 14:36:26 +0400

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import RealEstatePage from "./page/RealEstatePage.vue"
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div>
+  <Header></Header>
+    <div class="container pt-5">
+      <RealEstatePage></RealEstatePage>
+    </div>
+    
+  </div>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+import 'bootstrap-icons/font/bootstrap-icons.css';
+// Optional: for Bootstrap's JS-based components like modals, tooltips, etc.
+createApp(App).mount('#app')
