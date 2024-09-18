2b5474ea8af0aa62bd1ee9775fabb98c4570fa12
VazhaASP <vazhaaspanidze12@gmail.com>
2024-09-18 14:39:33 +0400

    feat - add selects on main page

---
+++ src/components/select/AreaSelect.vue
@@ -0,0 +1,24 @@
+<script setup lang="js">
+</script>
+
+<template>
+    <div class="btn-group me-3 p-1">
+        <button 
+          type="button" 
+          class="btn dropdown-toggle filter-dropdown-btn" 
+          data-bs-toggle="dropdown" 
+          data-bs-display="static" 
+          aria-expanded="false">
+          ფართობი
+        </button>
+        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+            <li><button class="dropdown-item" type="button">Action</button></li>
+            <li><button class="dropdown-item" type="button">Another action</button></li>
+            <li><button class="dropdown-item" type="button">Something else here</button></li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
