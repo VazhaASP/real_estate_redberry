5fc5954664d0ba69e213b2f02530f3cafdbb410f
VazhaASP <vazhaaspanidze12@gmail.com>
2024-09-21 20:25:47 +0400

    change vita config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/real-estate-redberry/',
+  base:'/real-estate-redberry/',
   plugins: [vue()],
 })
