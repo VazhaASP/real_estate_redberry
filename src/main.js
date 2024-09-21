42ef9e1206953ff7a0641211777c76960338eff4
VazhaASP <vazhaaspanidze12@gmail.com>
2024-09-21 21:20:03 +0400

    change vite config

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory ('/real-estate-redberry/'),
+    history: createWebHistory ('/real_estate_redberry/'),
     routes,
 })
 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/real-estate-redberry/',
+  base:'/real_estate_redberry/',
   plugins: [vue()],
 })
